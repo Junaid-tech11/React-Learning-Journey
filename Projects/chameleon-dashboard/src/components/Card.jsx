427b933d2782de6556772601726a2e449317cc82
Junaid <junaid8bp01@gmail.com>
2026-01-16 23:34:56 +0500

    Daily Progress: pracice

--- Projects/chameleon-dashboard/src/components/Card.jsx
+++ Projects/chameleon-dashboard/src/components/Card.jsx
@@ -9,13 +9,13 @@ export default function Card() {
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport.
                     </h5>
                 </a>
 
                 {/* Notice the dark: classes below! 👇 */}
                 <div className="flex items-center justify-between mt-4">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$799</span>
                     <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                 </div>
             </div>
